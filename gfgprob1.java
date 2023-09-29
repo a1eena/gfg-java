8a9fb3ad2580a2899fdaa885955286270318f2ff
Alina <112404368+a1eena@users.noreply.github.com>
2023-09-29 23:18:57 +0530

    Add files via upload

---
+++ gfgprob1.java
@@ -0,0 +1,14 @@
+//Searching an element in a sorted array
+//Given an array arr[] sorted in ascending order of size N and an integer K. Check if K is present in the array or not.
+class Solution{
+    static int searchInSorted(int arr[], int N, int K)
+    {
+             for(int i=0;i<N;i++)
+        {
+            if(arr[i]==K)
+           return 1; 
+        }
+        return -1;
+        
+    }
+}
